fe705c9c4b4f0383cafc4e1c9b16091b8a8b31ec
Priyatam-Vemulapalli <priyatamvemulapalli@gmail.com>
2024-10-27 11:23:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,22 @@
 //your code here!
+const infiList = document.getElementById("infi-list");
+let itemCount = 10;
+
+function addListItems(count) {
+  for (let i = 0; i < count; i++) {
+    const listItem = document.createElement("li");
+    listItem.textContent = `Item ${itemCount + 1}`;
+    infiList.appendChild(listItem);
+    itemCount++;
+  }
+}
+
+addListItems(10);
+
+infiList.addEventListener("scroll", () => {
+  if (infiList.scrollTop + infiList.clientHeight >= infiList.scrollHeight) {
+
+    addListItems(2);
+  }
+});
 
